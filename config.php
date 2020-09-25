a17b71972e4c479a6b07e9eac4fa277ff03880c7
Artem Golubchikov <artem@golubchikov.ml>
2020-09-25 10:53:26 +0300

    added js validation

--- config.php
+++ config.php
@@ -4,4 +4,16 @@ $db_login='login_mysql';
 $db_pass='password_for_database';
 $db_name='database_name';
 
+$salt='fY5C8kNePlSQ17z';
+
+
+$dsn = 'mysql:dbname='.$db_name.'testdb;host='.$db_server;
+
+
+try {
+    $dbh = new PDO($dsn, $db_login, $db_pass);
+} catch (PDOException $e) {
+    echo 'Database connection error: ' . $e->getMessage();
+}
+
 ?>
--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function id(e){return document.getElementById(e);}
 
 function check() {
-	
+	if (id('name').value=='') {
+		alert('Name in empty');
+		return false;
+	}
+	if (id('surname').value=='') {
+		alert('Surname is empty');
+		return false;
+	}
+	if (id('password').value.length<8) {
+		alert('Password too short');
+		return false;
+	}
+	if ((id('login').value.length<6) || (!id('login').value.match(/^[0-9a-zA-Z]+$/))) {
+		alert('Login too short or contains unsupported chars');
+		return false;
+	}
 	
 }
